d04c85dfe062e33af91bfa43024eecb62d8e55ca
Anne Kelly da Costa Geraldo <annekellyc@gmail.com>
2014-07-04 21:36:22 -0300

    learning sinatra - part II

--- main.rb
+++ main.rb
@@ -1,7 +1,37 @@
 require 'sinatra'
 require 'slim'
+require 'data_mapper'
+
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+
+class Task
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, String, :required => true
+  property :completed_at, DateTime
+end
+DataMapper.finalize
 
 get '/' do
-  @task = params[:task]
-  slim :task
+  @tasks = Task.all
+  slim :index
 end
+
+post '/' do
+  Task.create params[:task]
+  redirect to('/')
+end
+
+delete '/task/:id' do
+  Task.get(params[:id]).destroy
+  redirect to('/')
+end
+
+put '/task/:id' do
+  task = Task.get params[:id]
+  task.completed_at = task.completed_at.nil? ? Time.now : nil
+  task.save
+  redirect to('/')
+end
+
+
